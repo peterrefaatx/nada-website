1502aec27ec25d79fbfd1167339b64431ad8eea7
peterrefaatx <peterrefaatx@users.noreply.github.com>
2026-08-08 05:40:56 +0300

    Fix mobile gallery visibility threshold and mobile navbar crowding

--- js/common.js
+++ js/common.js
@@ -62,8 +62,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     }, {
-      threshold: 0.15,
-      rootMargin: '0px 0px -40px 0px'
+      threshold: 0.02,
+      rootMargin: '0px 0px -10px 0px'
     });
 
     fadeEls.forEach(el => fadeObserver.observe(el));
